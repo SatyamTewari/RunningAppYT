8c88770142b24431bd5c729c63a324df8d979479
SatyamTewari <satyamtewari13@gmail.com>
2022-08-01 20:33:14 +0530

    Integrating Map View with Tracking Fragment

--- app/src/main/java/com/example/runningappyt/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/runningappyt/fragments/TrackingFragment.kt
@@ -1,14 +1,62 @@
 package com.example.runningappyt.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.runningappyt.R
 import com.example.runningappyt.viewmodels.MainViewModel
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_tracking.*
 
 
 @AndroidEntryPoint
 class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private val viewModel : MainViewModel by viewModels()
+
+    private var map: GoogleMap? = null // google map instance with be shown over mapView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView.onCreate(savedInstanceState)
+
+        mapView.getMapAsync{
+            map = it
+            /* the above will simply crash if lifecycle not properly managed
+            since we aren't using mapsFragment we will have to handle the maps lifecycle */
+
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
 }
